16ad67e6c42c6deaced045996943477ead2c100d
Oliver Singer <owsinger@outlook.com>
2022-12-09 01:45:48 +0000

    implemented things1

--- things/models.py
+++ things/models.py
@@ -1,3 +1,6 @@
+import django
+django.setup()
+
 from django.db import models
 from django.core.exceptions import ValidationError
 
@@ -10,7 +13,7 @@ class Thing(models.Model):
             raise ValidationError("This value must be between 0 and 100")
         else:
             return value
-            
+
     name = models.CharField("name", unique=True, max_length=30, blank=False)
     description = models.TextField("description", max_length=120)
     quantity = models.IntegerField("quantity", max_length=3, blank=False, validators=[validate_quantity])
